963cce53a0d2f24b872f80b7a3ca58e5711df330
Imraan-Hendricks <imraan.hndx@gmail.com>
2022-09-27 01:06:21 +0200

    Added bold style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,6 +33,33 @@ const Home: NextPage = () => (
         </div>
       </div>
     </section>
+    <section className='px-5 py-24 bg-gray-50'>
+      <div className='container mx-auto grid gap-8 text-center'>
+        <h1 className='text-title'>Typography - bold</h1>
+        <div className='grid gap-8'>
+          <h1 className='text-display-bold'>Text Display</h1>
+          <h2 className='text-title-bold'>Text Title</h2>
+          <h3 className='text-subtitle-bold'>Text Subtitle</h3>
+          <h4 className='text-h1-bold'>H1. Heading One</h4>
+          <h5 className='text-h2-bold'>H2. Heading Two</h5>
+          <h6 className='text-h3-bold'>H3. Heading Three</h6>
+          {[
+            'text-largeprint-bold',
+            'text-body-bold',
+            'text-fineprint-bold',
+          ].map((className) => (
+            <p
+              key={className}
+              className={twMerge('max-w-[48rem] mx-auto', className)}>
+              {className.substring(5)}.{' '}
+              <span className='text-gray-500'>{paragaph}</span>
+            </p>
+          ))}
+          <span className='text-button-bold'>Button Text</span>
+          <p className='text-caption-bold'>caption text</p>
+        </div>
+      </div>
+    </section>
   </main>
 );
 
